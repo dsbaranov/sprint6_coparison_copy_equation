a54ddd2e4104708ca357309b950fb26596afe616
dsbaranov <DmitryTheRedHead@yandex.ru>
2024-07-19 01:20:17 +0300

    mistaken

---
+++ build/CMakeFiles/comparison_copy_equation5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/comparison_copy_equation5.dir/main.cpp.o"
+  "CMakeFiles/comparison_copy_equation5.dir/main.cpp.o.d"
+  "comparison_copy_equation5"
+  "comparison_copy_equation5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/comparison_copy_equation5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
